ae33d8ea8f595ace97f8d72c541b863816e306a0
Kevin Shofner <kshofner@csumb.edu>
2018-08-29 22:04:56 +0000

    adding lab 1 files

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<html><body>
-<?php
+<!DOCTYPE html>
+
+<html>
+    <title> Kevin Shonfer's </title>
+    <body>
+        <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
