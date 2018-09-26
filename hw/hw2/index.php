a6468ba4be060da68ccf15fb56d65df8c9e10617
Kevin Shofner <kshofner@csumb.edu>
2018-09-26 04:14:31 +0000

    had to make changes to img folder

--- hw/hw2/index.php
+++ hw/hw2/index.php
@@ -59,6 +59,8 @@ function chooseMeal($time){
     global $foods;
     $amount = count($foods); // 3rd array function 
     echo "<h1 id=topText> Out of  $amount food choices, 3 food and drink choices will be made!</h1>";
+    echo "<br>";
+    
 
     $time = 0;
         chooseMeal($time);
@@ -91,6 +93,36 @@ function chooseMeal($time){
     
     <footer>
         <br>
+        <br>
+        <br>
+        
+         <details>
+                <summary>Citations:</summary>
+                
+ <a href ="https://static1.squarespace.com/static/583ca2f2d482e9bbbef7dad9/58863361d2b857a1707bf488/588677d403596ebc5f1d798b/1534376808162/iStock-5078775151900.jpg?format=1500w"> water.jpg</a>
+<br>
+<a href ="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG"> coffee.jpg</a>
+<br>
+<a href ="https://us-east-1.tchyn.io/snopes-production/uploads/2018/08/gatoradead.jpg"> gatorade.jpg</a>
+<br>
+<a href ="https://cdn.shopify.com/s/files/1/1752/6725/products/smokea-stash-cans-smokea-dr-pepper-stash-can-20673625409_1400x.jpg?v=1518055462"> Dr.Pepper</a>
+<br>
+<a href ="https://target.scene7.com/is/image/Target/12953529?wid=488&hei=488&fmt=pjpeg"> coke.jpg</a>
+<br>
+<a href ="https://www.incredibleegg.org/wp-content/uploads/basic-french-omelet-930x550.jpg"> omelette.jpg</a>
+<br>
+<a href ="https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg"> pizza.jpg</a>
+<br>
+<a href = "https://upload.wikimedia.org/wikipedia/commons/9/94/Salad_platter.jpg"> salad.jpg</a>
+<br>
+<a href = "https://hips.hearstapps.com/del.h-cdn.co/assets/18/08/1519155106-flank-steak-horizontal.jpg">steak.jpg</a>
+<br>
+<a href ="https://cdn-image.foodandwine.com/sites/default/files/styles/medium_2x/public/201403-xl-pineapple-teriyaki-glazed-chicken-wings.jpg?itok=HQJJGht_">wings.jpg</a>
+<br>
+
+            </details>
+        
+        <strong>Note: You can drink the same thing throughout the day, but you can't eat the same thing all day.</strong>
         <br>
         <br>
         <img src ="img/buddy_verified.png" id="buddy" alt = "Buddy_verification" title="Buddy_verification">
