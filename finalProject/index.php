025a99c63f2828acd16b58a696fa4b018b6aad31
Kevin Shofner <kshofner@csumb.edu>
2018-12-01 22:22:33 +0000

    forgot to add something

---
+++ finalProject/index.php
@@ -0,0 +1,21 @@
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Admin or User Login </title>
+    </head>
+    <body id = "indexBody">
+
+        <h1> Admin or User Login </h1>
+        
+        
+        
+                        <li><a href = "user.php">User Search</a></li>
+                        <br>
+                        <li><a href = "adminLogin.php">Admin Login</a></li>
+
+
+    </body>
+</html>
